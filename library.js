45aba11f3134f134b4ff06fbb8f56d132f562e2a
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-02-15 19:54:01 -0700

    removed the prompt to re enter text if string is too short

--- library.js
+++ library.js
@@ -12,11 +12,10 @@ letterCount = number.length;
 console.log(letterCount)
 if (letterCount < 5) {
 alert("enter more text please")
-var number = prompt("Type Something")
 }
 else {
 number = number.split('').reverse().join('')
 alert(number)
-
+console.log(number)
 }
 }
