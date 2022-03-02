26524b288372248c064818da52190bd03c87bc19
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-03-02 10:11:29 -0700

    Update library.js

--- library.js
+++ library.js
@@ -1,34 +1,26 @@
-function doStuff(){
+function doStuff() {
     showAlert()
 }
-
-
-
 function showAlert() {
+    var number = prompt("Type Something, except racecar")
+    console.log(number)
+    letterCount = number.length;
+    console.log(letterCount)
+    if (letterCount < 5) {
+        alert("enter more text please")
+    }
+    else {
+        var vowels = "aeiou";
+        var result = ""
 
-var number = prompt("Type Something, except racecar")
-console.log(number)
-letterCount = number.length;
-console.log(letterCount)
-if (letterCount < 5) {
-alert("enter more text please")
-}
-else {
-    var vowels = "aeiou";
-var result = ""
-var vowelsLength = vowels.length;
-    result += vowels.charAt(Math.floor(Math.random() * vowelsLength));
-}
+        console.log(result)
+        var testy = '';
+        for (const brokenDownString of number) {
 
-console.log(result)
-    var testy = '';
-    var chosenOne = result
-    console.log(chosenOne)
-for(const brokenDownString of number){
-    
-    console.log(brokenDownString);
-    testy += brokenDownString + chosenOne
- 
-}
-alert(testy)
+            console.log(brokenDownString);
+            testy += brokenDownString + vowels.charAt(Math.floor(Math.random() * 4));
+
+        }
+        alert(testy)
+    }
 }
